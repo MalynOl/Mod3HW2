43d2507e124019347c23aa03995b64114a2cee62
Aleksandra <oleksandramalyn@gmail.com>
2022-02-18 13:53:44 +0200

    group contacts list

---
+++ HW11Contact/HW11Contact/Providers/GetAllContacts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11Contact
+{
+    public class GetAllContacts
+    {
+        public GetAllContacts()
+        {
+            ListContacts = new List<Contact>
+            {
+                new Contact("Adamson", "Tom", " ", "020 4565 1232"),
+                new Contact("Smith", "Kevin", " ", "020 1234 1232"),
+                new Contact("Walker", "Ben", " ", "020 4565 4422"),
+                new Contact("Lewis", "Alexander", " ", "020 4465 1235"),
+                new Contact("Adamson", "Karl", " ", "020 7878 1232"),
+                new Contact("Davis", "Tom", " ", "020 4465 1235"),
+                new Contact("Walkers", "Kevin", " ", "720 4442 4422"),
+                new Contact("вечер", "Сергей", "Леонидович", "050 4565 132"),
+                new Contact("Івасюк", "Данило", "Вікторович", "050 1165 132"),
+                new Contact("Кулик", "Остап", "Богданович", "050 4565 456"),
+                new Contact("Дорошенко", "Егор", "Александрович", "050 7765 552"),
+                new Contact("Доронин", "Иван", "Александрович", "050 1212 552"),
+                new Contact("Зелений", "Николай", "Сергеевич", "066 9965 162"),
+                new Contact("1255", " ", " ", "098 9965 162"),
+                new Contact("  ", "Сергей", " ", "066 1111 162"),
+                new Contact("125Простенко", "Сергей", "Сергеевич", "066 9965 162"),
+                new Contact("+Ново", " ", " ", "066 1111 162"),
+                new Contact("+++", " ", " ", "066 9965 111"),
+            };
+        }
+
+        public List<Contact> ListContacts { get; set; }
+    }
+}
---
+++ HW11Contact/HW11Contact/Services/Service.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11Contact
+{
+    public class Service
+    {
+        public Service()
+        {
+            AllContacts = new GetAllContacts();
+        }
+
+        public GetAllContacts AllContacts { get; init; }
+
+        public Dictionary<string, List<Contact>> MethodUK() // [А-ЯҐЄІЇ]
+        {
+            string patternUK = @"[А-ЯҐЄІЇа-яґєії]";
+
+            List<Contact> contactsUKList = GetListContactsForPattern(patternUK);
+
+            Dictionary<string, List<Contact>> ukContacts = AddToDictionary(contactsUKList);
+
+            List<Contact> contactsDigitList = MethodDigitContacts();
+
+            ukContacts.Add("[0-9]", contactsDigitList);
+
+            string patternOther = @"\W|[a-zA-Z]";
+
+            List<Contact> contactsOtherSymbolList = MethodOtherSymbol(patternOther);
+
+            ukContacts.Add("#", contactsOtherSymbolList);
+
+            return ukContacts;
+        }
+
+        public Dictionary<string, List<Contact>> MethodRU()
+        {
+            string patternRU = @"[А-Яа-я]";
+
+            List<Contact> contactsRUList = GetListContactsForPattern(patternRU);
+
+            Dictionary<string, List<Contact>> ruContacts = AddToDictionary(contactsRUList);
+
+            List<Contact> contactsDigitList = MethodDigitContacts();
+
+            ruContacts.Add("[0-9]", contactsDigitList);
+
+            string patternOther = @"\W|[a-zA-ZҐЄІЇ]";
+
+            List<Contact> contactsOtherSymbolList = MethodOtherSymbol(patternOther);
+
+            ruContacts.Add("#", contactsOtherSymbolList);
+
+            return ruContacts;
+        }
+
+        public Dictionary<string, List<Contact>> MethodEN()
+        {
+            string patternEn = @"[a-zA-Z]";
+
+            List<Contact> contactsENList = GetListContactsForPattern(patternEn);
+
+            Dictionary<string, List<Contact>> enContacts = AddToDictionary(contactsENList);
+
+            List<Contact> contactsDigitList = MethodDigitContacts();
+
+            enContacts.Add("[0-9]", contactsDigitList);
+
+            string patternOther = @"\W|[А-ЯҐЄІЇ]";
+
+            List<Contact> contactsOtherSymbolList = MethodOtherSymbol(patternOther);
+
+            enContacts.Add("#", contactsOtherSymbolList);
+
+            return enContacts;
+        }
+
+        public List<Contact> MethodDigitContacts()
+        {
+            string pattern = @"^\d";
+
+            List<Contact> contactsDigitList = GetListContactsForPattern(pattern);
+
+            return contactsDigitList;
+        }
+
+        public List<Contact> MethodOtherSymbol(string pattern)
+        {
+            List<Contact> contactSymbolList = GetListContactsForPattern(pattern);
+
+            return contactSymbolList;
+        }
+
+        public List<Contact> GetListContactsForPattern(string pattern)
+        {
+            List<Contact> outputContacs = new List<Contact>();
+            try
+            {
+                if (AllContacts != null)
+                {
+                    AllContacts.ListContacts.Sort();
+
+                    foreach (var item in AllContacts.ListContacts)
+                    {
+                        if (Regex.IsMatch(item.FullName.Substring(0, 1), pattern))
+                        {
+                            outputContacs.Add(item);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return outputContacs;
+        }
+
+        public Dictionary<string, List<Contact>> AddToDictionary(List<Contact> contacts)
+        {
+            Dictionary<string, List<Contact>> cultureDictionary = new Dictionary<string, List<Contact>>();
+
+            try
+            {
+                if (contacts != null)
+                {
+                    foreach (var item in contacts)
+                    {
+                        string firstLatter = item.FullName.Substring(0, 1);
+
+                        if (!cultureDictionary.ContainsKey(firstLatter))
+                        {
+                            var selectedContacts = (from c in contacts
+                                                    where c.LastName.StartsWith(firstLatter)
+                                                    select c).ToList<Contact>();
+
+                            cultureDictionary.Add(firstLatter.ToUpper(), selectedContacts);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return cultureDictionary;
+        }
+    }
+}
